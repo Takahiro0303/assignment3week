84218d70e7575d8823894455387e04e9a0e19360
takahiro tshuchida <taka020303@gamil.com>
2017-08-07 22:20:35 +0800

    20170807進捗

---
+++ index.php
@@ -0,0 +1,69 @@
+<?php
+
+
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" href="assets/css/bootstrap.css">
+<link rel="stylesheet" href="assets/font-awesome/css/font-awesome.css">
+<link rel="stylesheet" type="text/css" href="index.css">
+	<title>Nexeed Training</title>
+</head>
+<body>
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12"><h1 class="header">NxSeed Diary</h1></div>
+			<div class="col-sm-2">
+				<div class="main-left">
+					<p class="konnitiwa">こんにちわ、ゲストさん</p>
+					<hr>
+					<a href="" class="date">2017年8月の日記</a>
+					<hr>
+					<a href="" class="date">2017年9月の日記</a>
+					<hr>
+					<a href="" class="date">2017年10月の日記</a>
+					<hr>
+				</div>
+			</div>
+			<div class="col-sm-10">
+				<div class="main-right">
+					<a href=""><h4>こんにちわ</h4></a>
+					<p>2017年8月10日</p>
+				</div>
+				<div class="main-right">
+					<a href=""><h4>こんにちわ</h4></a>
+					<p>2017年8月10日</p>
+				</div>
+				<div class="main-right">
+					<a href=""><h4>こんにちわ</h4></a>
+					<p>2017年8月10日</p>
+				</div>
+				<div class="main-right">
+					<a href=""><h4>こんにちわ</h4></a>
+					<p>2017年8月10日</p>
+				</div>
+				<div class="main-right">
+					<a href=""><h4>こんにちわ</h4></a>
+					<p>2017年8月10日</p>
+				</div>
+				<div class="main-right">
+					<a href=""><h4>こんにちわ</h4></a>
+					<p>2017年8月10日</p>
+				</div>
+			</div>
+		<div class="col-sm-12">
+			<div class="footer">Copyright NexSeed inc All Rights Reserved.</div>
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
